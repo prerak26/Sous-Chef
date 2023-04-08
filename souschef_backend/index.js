852d8b87158418bf6d772c544a349f0923937135
adityakadoo <aditya1449kadoo@gmail.com>
2023-04-08 17:48:53 +0530

    get and create recipe added

--- souschef_backend/index.js
+++ souschef_backend/index.js
@@ -40,74 +40,49 @@ app.get('/', (req, res) => {
 app.post('/login', (req, res) => {
   model.getChef(req.body.id)
     .then(response => {
-      console.log(response);
-      if ( (response.length !== 0 && bcrypt.compareSync(req.body.pswd, response[0].hashedpassword)) 
-        || (req.body.id === process.env.user && req.body.pswd === process.env.password) ) {
+      if ((response.length !== 0 && bcrypt.compareSync(req.body.pswd, response[0].hashedpassword))
+        || (req.body.id === process.env.user && req.body.pswd === process.env.password)) {
         session = req.session;
         session.userid = req.body.id;
-        res.status(200).send(`Hey there, welcome <a href=\'/logout'>click to logout</a>`);
+        res.status(200).send({ message: "Logged in successfully" });
       }
       else {
-        res.status(403).send('Invalid username or password');
+        res.status(404).send({ message: "Invalid username or password" });
       }
     })
     .catch(error => {
       res.status(500).send(error);
     });
-})
+});
 
 app.post('/signup', (req, res) => {
   model.getChef(req.body.id)
-  .then(
-    chefList => {
-      if(chefList.length !== 0) {
-        res.status(403).send('Username already exists');
-      }else{
+    .then(chefList => {
+      if (chefList.length !== 0) {
+        res.status(403).send({ message: "Username already exists" });
+      } else {
         model.createChef(req.body.id, req.body.name, bcrypt.hashSync(req.body.pswd, saltRounds))
-        .then(response => {
-          session = req.session;
-          session.userid = req.body.id;
-          res.status(200).send(req.body.id);
-        })
-        .catch(error => {
-          res.status(500).send(error);
-        });
-      }
-    }
-  );
-})
-
-app.get('/chef/:id', (req, res) => {
-  session = req.session;
-  console.log(session.userid);
-  if (session.userid) {
-    model.getChef(req.params.id)
-    .then(response => {
-      if ( (response.length !== 0) ) {
-        let filteredResponse = {
-          chefId: response[0].chefid,
-          name: response[0].name
-        };
-        res.status(200).send(filteredResponse);
-      }
-      else {
-        res.status(404).send('Chef not found');
+          .then(response => {
+            session = req.session;
+            session.userid = req.body.id;
+            res.status(200).send(req.body.id);
+          })
+          .catch(error => {
+            res.status(500).send(error);
+          });
       }
     })
     .catch(error => {
       res.status(500).send(error);
     });
-  }else{
-    res.status(401).send('Please <a href=\'/\'>login</a> first');
-  }
-})
+});
 
 app.get('/auth', (req, res) => {
   session = req.session;
-  if (session.userid) {
+  if (session.userid)
     res.status(200).send(session.userid);
-  } else
-    res.status(401).send('Please <a href=\'/index\'>login</a> first');
+  else
+    res.status(401).send({ message: "Please login first" });
 })
 
 app.get('/logout', (req, res) => {
@@ -115,6 +90,81 @@ app.get('/logout', (req, res) => {
   res.redirect('/');
 });
 
+app.get('/chef/:id', (req, res) => {
+  model.getChef(req.params.id)
+    .then(response => {
+      if ((response.length !== 0)) {
+        let filteredResponse = {
+          chefId: response[0].chefid,
+          name: response[0].name
+        };
+        res.status(200).send(filteredResponse);
+      }
+      else {
+        res.status(404).send({ message: "Chef not found" });
+      }
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    });
+});
+
+app.get('/recipe/:id', (req, res) => {
+  session = req.session;
+  model.getRecipe(parseInt(req.params.id))
+    .then(response => {
+      if ((response.length !== 0)) {
+        reqRecipe = response[0];
+        if (reqRecipe.visibility === 'private' && session.userid !== reqRecipe.authorid)
+          res.status(403).send({ message: "Unauthorized access" });
+        else
+          res.status(200).send(reqRecipe);
+      }
+      else
+        res.send(404).send({ message: "Recipe not found" });
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    });
+});
+
+app.post('/recipe', (req, res) => {
+  session = req.session;
+  if (session.userid)
+    model.getRecipeId()
+      .then(recipeId => {
+        model.createRecipe(recipeId, req.body.title, parseInt(req.body.serves),
+          (req.body.isPublic ? 'public' : 'private'), session.userid)
+          .then(recipeResponse => {
+            for (let i = 0; i < req.body.steps.length; i++)
+              model.createStep(recipeId, i + 1, req.body.steps[i].desc, req.body.steps[i].duration)
+                .then(stepResponse => {
+                  for (let j = 0; j < req.body.steps[i].ingredients.length; j++)
+                    model.createRequirement(recipeId, i + 1, j + 1,
+                      req.body.steps[i].ingredients[j].id,
+                      req.body.steps[i].ingredients[j].quantity)
+                      .then(requirementResponse => {})
+                      .catch(error => {
+                        res.status(500).send(error);
+                      });
+                })
+                .catch(error => {
+                  res.status(500).send(error);
+                });
+            res.status(200).send({ message: "New recipe added" });
+          })
+          .catch(error => {
+            res.status(500).send(error);
+          });
+      })
+      .catch(error => {
+        console.log(session);
+        res.status(500).send(error);
+      });
+  else
+    res.status(401).send({ message: "Login first" });
+});
+
 app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-  });
+  console.log(`App running on port ${port}.`)
+});
--- souschef_backend/model.js
+++ souschef_backend/model.js
@@ -12,10 +12,10 @@ const getChef = (id) => {
   const ID = id;
   return new Promise((resolve, reject) => {
     pool.query('SELECT * FROM Chefs WHERE chefId=$1', [ID], (error, results) => {
-      if (error) {
+      if (error)
         reject(error);
-      }
-      resolve(results.rows);
+      if (results)
+        resolve(results.rows);
     })
   })
 }
@@ -26,16 +26,87 @@ const createChef = (id, name, pswd) => {
   const PSWD = pswd;
   return new Promise((resolve, reject) => {
     pool.query('INSERT INTO Chefs (chefId, name, hashedPassword) VALUES ($1, $2, $3)', [ID, NAME, PSWD], (error, results) => {
-      console.log(results);
-      if (error) {
+      if (error)
         reject(error);
-      }
-      resolve(results.rows);
+      if (results)
+        resolve(results.rows);
     })
+  })
+}
+
+const getRecipe = (id) => {
+  const ID = id;
+  return new Promise((resolve, reject) => {
+    pool.query('SELECT * FROM Recipes where recipeId=$1', [ID], (error, results) => {
+      if (error)
+        reject(error);
+      if (results)
+        resolve(results.rows);
+    })
+  })
+}
+
+const getRecipeId = () => {
+  return new Promise((resolve, reject) => {
+    pool.query('SELECT count(*) FROM Recipes', (error, results) => {
+      if (error)
+        reject(error);
+      if (results)
+        resolve(parseInt(results.rows[0].count));
+    })
+  })
+}
+
+const getDateTime = () => {
+  const now = new Date();
+  return now.getFullYear() + "-" + now.getMonth() + "-" + now.getDate() + " " +
+    now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds() + "." +
+    now.getMilliseconds();
+}
+
+const createRecipe = (recipeId, title, serves, visibility, authorId) => {
+  const LASTMODIFIED = getDateTime();
+  return new Promise((resolve, reject) => {
+    pool.query('INSERT INTO Recipes (recipeId, title, serves, visibility, authorId, lastModified) VALUES ($1, $2, $3, $4, $5, $6)',
+      [recipeId, title, serves, visibility, authorId, LASTMODIFIED], (error, results) => {
+        if (error)
+          reject(error);
+        if (results)
+          resolve(results.rows);
+      })
+  })
+}
+
+const createStep = (recipeId, stepNumber, desc, duration) => {
+  return new Promise((resolve, reject) => {
+    pool.query('INSERT INTO Steps (recipeId, stepNumber, description, duration) VALUES ($1, $2, $3, $4)',
+      [recipeId, stepNumber, desc, duration], (error, results) => {
+        if (error)
+          reject(error);
+        if (results)
+          resolve(results.rows);
+      })
+  })
+}
+
+const createRequirement = (recipeId, stepNumber, serialNumber, ingredientId, quantity) => {
+  return new Promise((resolve, reject) => {
+    pool.query('INSERT INTO Requirements (recipeId, stepNumber, serialNumber, ingredientId, quantity) VALUES ($1, $2, $3, $4, $5)',
+      [recipeId, stepNumber, serialNumber, ingredientId, quantity], (error, results) => {
+        if (error)
+          reject(error);
+        if (results)
+          resolve(results.rows);
+      })
   })
 }
 
 module.exports = {
   getChef,
-  createChef
+  createChef,
+  getRecipe,
+  getRecipeId,
+  createRecipe,
+  createStep,
+  createRequirement
 }
